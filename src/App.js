002af463f8e96084d5b719f5bec02b8123e0456a
JavierMartinezOliveros <jmo.ilustrador@gmail.com>
2020-03-30 06:59:29 -0500

    adding files for github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; //Component trae la clase
+//import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.components';
+import { SearchBox } from './components/search-box/search-box.components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      heroes : [ ],
+      searchField : ''
+    };
+
+  }
+
+  componentDidMount() {
+    //fetch('https://superheroapi.com/api/10157298358717950/id')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ heroes: users }));
+  }
+
+  handleChange = (e) =>{
+    this.setState({ searchField: e.target.value})
+  }
+
+  render () {
+    const { heroes, searchField } = this.state;
+    const filteredHeroes = heroes.filter( hero => hero.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+    return(
+      <div className="App">
+        <h1> Monster Rolodex </h1>
+        <SearchBox
+          placeholder = 'search heroes'
+          handleChange = {this.handleChange}
+        />
+        <CardList heroes={filteredHeroes} />
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+
