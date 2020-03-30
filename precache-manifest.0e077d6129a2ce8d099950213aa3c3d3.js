174b8d5c8a83cdb26ef348469e6841b7bb23a29c
JavierMartinezOliveros <jmo.ilustrador@gmail.com>
2020-03-30 06:58:48 -0500

    Updates

---
+++ precache-manifest.0e077d6129a2ce8d099950213aa3c3d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3d8b6b039b34642830cd2788402840",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "6ae88df5827c52466aaf",
+    "url": "/rolodex/static/css/main.6ec2571a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ae88df5827c52466aaf",
+    "url": "/rolodex/static/js/main.5985cb16.chunk.js"
+  },
+  {
+    "revision": "d72009913ef41833d3a3",
+    "url": "/rolodex/static/js/runtime-main.2fc8d5fe.js"
+  },
+  {
+    "revision": "6fac055f68f01528dfb4091e33ce5662",
+    "url": "/rolodex/static/media/cool-background.6fac055f.png"
+  }
+]);
