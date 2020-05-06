e778ad5ce5c5cb1a86a92103ea97d6c0a814bf0a
JavierMartinezOliveros <jmo.ilustrador@gmail.com>
2020-05-06 18:59:25 -0500

    now with the marvel comics  api

--- src/App.js
+++ src/App.js
@@ -5,44 +5,59 @@ import { SearchBox } from './components/search-box/search-box.components';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      heroes : [ ],
-      searchField : ''
-    };
+		this.state = {
+			heroes: [],
+			heroesMarvel: [],
+			searchField: ''
+		};
 
-  }
+	}
 
-  componentDidMount() {
-    //fetch('https://superheroapi.com/api/10157298358717950/id')
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ heroes: users }));
-  }
+	componentDidMount() {
+		//fetch('https://superheroapi.com/api/10157298358717950/id')
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ heroes: users }));
 
-  handleChange = (e) =>{
-    this.setState({ searchField: e.target.value})
-  }
+		// fetch('http://gateway.marvel.com//v1/public/characters?limit=100&ts=1&apikey=c46197f79f373ff6b5c458314d26732f&hash=972d7c7ecaed6fe18b83e6ac8c04e0af')
+		fetch('http://gateway.marvel.com/v1/public/series/8842/characters?limit=100&ts=1&apikey=c46197f79f373ff6b5c458314d26732f&hash=972d7c7ecaed6fe18b83e6ac8c04e0af')
+			.then(response => response.json())
+			.then(res => {
+				console.log("Here is loading the marvel characters");
+				const heroesMarvel = res.data.results
+				console.log(heroesMarvel);
+				this.setState({
+					heroesMarvel
+				})
+			})
+		// .then(users => this.setState({ heroes: users }));
 
-  render () {
-    const { heroes, searchField } = this.state;
-    const filteredHeroes = heroes.filter( hero => hero.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
-    return(
-      <div className="App">
-        <h1> Monster Rolodex </h1>
-        <SearchBox
-          placeholder = 'search heroes'
-          handleChange = {this.handleChange}
-        />
-        <CardList heroes={filteredHeroes} />
-      </div>
-    )
-  }
+
+
+	}
+
+	handleChange = (e) => {
+		this.setState({ searchField: e.target.value })
+	}
+
+	render() {
+		const { searchField, heroesMarvel } = this.state;
+		const filteredHeroes = heroesMarvel.filter(hero => hero.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+
+		return (
+			<div className="App">
+				<h1> Monster Rolodex </h1>
+				<SearchBox
+					placeholder='search heroes'
+					handleChange={this.handleChange}
+				/>
+				<CardList heroes={filteredHeroes} />
+			</div>
+		)
+	}
 }
 
 export default App;
-
-
-
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = props => (
-    <div className='card-container'>
-        <img alt="hero" src={`https://robohash.org/${props.hero.id}?set=set1&size=180x180`} />
-        <h2> { props.hero.name } </h2>
-        <p> { props.hero.email} </p>
-    </div>
+	<div className='card-container'>
+		<div className="thumb-wrraper">
+			<img alt="hero" src={`${props.hero.thumbnail.path}.${props.hero.thumbnail.extension}`} />
+		</div>
+		<h2> {props.hero.name} </h2>
+		<p> {props.hero.email} </p>
+	</div>
 )
