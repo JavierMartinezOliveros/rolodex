f8f9390f5ecf4e127164cce7a24395e4cd89184c
JavierMartinezOliveros <jmo.ilustrador@gmail.com>
2020-05-06 19:11:47 -0500

    let see if this gets publish

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<h1> Monster Rolodex </h1>
+				<h1> Heroes Rolodex </h1>
 				<SearchBox
 					placeholder='search heroes'
 					handleChange={this.handleChange}
